d2c8d0d1607c48fb842e4727b1e7676351b24e6c
yoavhacmon <89570670+yoavhacmon@users.noreply.github.com>
2021-08-29 20:54:00 +0300

    absulote_number

--- Quadratic_Equation.js
+++ Quadratic_Equation.js
@@ -2,6 +2,6 @@ const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
 let solutionA = solutionB = 0
 // your code goes here
 
-solutionA = (-b+Math.sqrt((b**2)-(4*a*c)))/2*a
-solutionB = (-b-Math.sqrt((b**2)-(4*a*c)))/2*a
-console.log(solutionA, solutionB)
+solutionA = (-b+Math.sqrt((b**2)-(4*a*c)))/2*a;
+solutionB = (-b-Math.sqrt((b**2)-(4*a*c)))/2*a;
+console.log(solutionA, solutionB);
---
+++ absolute_number.js
@@ -0,0 +1,12 @@
+const num1 = -8;
+
+let absolute;
+// your code goes here
+if (num1 < 0) {
+   absolute = -1*num1
+} 
+else {
+    absolute = num1
+}
+
+console.log(absolute)
