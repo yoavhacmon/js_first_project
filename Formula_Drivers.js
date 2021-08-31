b00c36649d7c843fa1fb8aadbfd33a7de9373711
yoavhacmon <89570670+yoavhacmon@users.noreply.github.com>
2021-08-31 20:58:01 +0300

    Formula_Drivers

---
+++ Formula_Drivers.js
@@ -0,0 +1,38 @@
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {
+
+}
+
+function countWin(driver) {
+    if (winners[driver]) {
+        winners[driver] += 1;
+    }
+    else {
+        winners[driver] = 1
+    }
+}
+
+formula1Champions.forEach(countWin);
+console.log(winners);
